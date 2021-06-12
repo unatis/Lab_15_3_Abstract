c27ff91fcdf9c496a7a2e1d0c88aa8a884b866b4
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-12 13:03:50 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Track track = new Track();
+
+        PassengerCar pCar = new PassengerCar();
+
+        Bike bike = new Bike();
+
+        bike.SignalSound();
+
+    }
+
+}
