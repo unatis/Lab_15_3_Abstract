b9d734a89cb96eaa8699a6650bbffb6b6db84b9d
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-12 13:04:54 +0200

    Create PassengerCar.java

---
+++ PassengerCar.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class PassengerCar extends Car{
+
+
+    @Override
+    public void OpenDoor() {
+
+    }
+
+    @Override
+    public void CloseDoor() {
+
+    }
+
+    @Override
+    public void Break() {
+
+    }
+}
